e306e88be11198c52f03a4ca24218d9670a52fda
Alyona <alyona.ivanova.kh@gmail.com>
2022-12-12 11:26:29 +0100

    set up eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+    plugins: [
+        'react',
+        '@typescript-eslint',
     ],
-    "rules": {
-    }
-}
+    rules: {
+        'import/no-default-export': 'off',
+    },
+};
